b67391d2e8eacf4769a0a7568481c4aca90ebec5
marianagonzl <10193@portalcedi.edu.mx>
2025-04-08 10:57:15 -0600

    plugins

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { Camera, CameraResultType } from '@capacitor/camera';
+import { Network } from '@capacitor/network';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class HomePage {
   constructor() {}
+
+  takePhoto = async () => {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+    });
+    
+
+    Network.addListener('networkStatusChange', status => {
+      console.log('Network status changed', status);
+    });
+    
+    const logCurrentNetworkStatus = async () => {
+      const status = await Network.getStatus();
+    
+      console.log('Network status:', status);
+    };
+    console.log(image.dataUrl);
+  };
+
 }
+
+
